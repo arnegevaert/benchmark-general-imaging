68382790962f03d8bc1d3e6813707239115a654a
Arne Gevaert <arnegevaert@gmail.com>
2021-10-11 11:16:03 +0200

    refactor plot code

--- plot/inter_metric_correlations.py
+++ plot/inter_metric_correlations.py
@@ -1,51 +1,17 @@
-import argparse
-from attrbench.suite import SuiteResult
 from attrbench.suite.plot import InterMetricCorrelationPlot
-from .dfs import get_default_dfs, get_all_dfs
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-from os import path
-import os
-import glob
-from tqdm import tqdm
+from dfs import get_default_dfs, get_all_dfs
 
 
-def inter_metric_correlations(result_files, out_dir, metric_selection="default"):
-    prog = tqdm(result_files)
-    for file in prog:
-        basename = path.basename(file)
-        ds_name, ext = basename.split(".")
-        prog.set_postfix_str(ds_name)
-        mode = "raw"
-        res_obj = SuiteResult.load_hdf(file)
-        if all:
-            dfs = get_all_dfs(res_obj, mode=mode)
-        else:
-            dfs = get_default_dfs(res_obj, mode=mode)
+def inter_metric_correlations(res_obj, metric_selection="default"):
+    if all:
+        dfs = get_all_dfs(res_obj, mode="raw")
+    else:
+        dfs = get_default_dfs(res_obj, mode="raw")
 
-        figsize = (17, 17) if metric_selection == "all" else (7, 7)
-        fontsize = 15 if metric_selection == "all" else 17
-        fig = InterMetricCorrelationPlot(dfs).render(figsize=figsize, annot=metric_selection == "all")
-        ax = fig.axes[0]
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor", fontsize=fontsize)
-        ax.set_yticklabels(ax.get_yticklabels(), fontsize=fontsize)
-        fig.savefig(path.join(out_dir, f"{ds_name}.png"), bbox_inches="tight", dpi=250)
-        plt.close(fig)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("in_dir", type=str)
-    parser.add_argument("out_dir", type=str)
-    parser.add_argument("--all", action="store_true")
-    args = parser.parse_args()
-
-    mpl.use("Agg")
-    np.seterr(all="raise")
-
-    if not path.isdir(args.out_dir):
-        os.makedirs(args.out_dir)
-
-    result_files = glob.glob(path.join(args.in_dir, "*.h5"))
-    inter_metric_correlations(result_files, args.out_dir, args.all)
+    figsize = (17, 17) if metric_selection == "all" else (7, 7)
+    fontsize = 15 if metric_selection == "all" else 17
+    fig = InterMetricCorrelationPlot(dfs).render(figsize=figsize, annot=metric_selection == "all")
+    ax = fig.axes[0]
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor", fontsize=fontsize)
+    ax.set_yticklabels(ax.get_yticklabels(), fontsize=fontsize)
+    return fig
--- plot/krip.py
+++ plot/krip.py
@@ -1,15 +1,7 @@
-import argparse
 import pandas as pd
 from attrbench.lib import krippendorff_alpha
-from attrbench.suite import SuiteResult
 from dfs import get_default_dfs, get_all_dfs
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
-from os import path
-import glob
-import seaborn as sns
-from tqdm import tqdm
 
 
 def compute_krippendorff_alpha(result_object, metric_selection="default"):
@@ -23,13 +15,16 @@ def compute_krippendorff_alpha(result_object, metric_selection="default"):
     return pd.Series(k_a)
 
 
-def plot_krippendorff_alpha(k_a: pd.DataFrame, color=None):
-    num_rows = k_a.shape[0] // 12 + 1
+def plot_krippendorff_alpha(k_a: pd.DataFrame, color=None, groups_per_row=14):
+    num_rows = (k_a.shape[0] - 1) // groups_per_row + 1
     fig, axs = plt.subplots(nrows=num_rows, figsize=(10, 4*num_rows), constrained_layout=True)
 
+    if num_rows == 1:
+        axs = [axs]
+
     for i in range(num_rows):
-        end = min((i+1)*12, k_a.shape[0])
-        k_a[i*12:end].plot.bar(ax=axs[i], color=color, width=0.7)
+        end = min((i+1)*groups_per_row, k_a.shape[0])
+        k_a[i*groups_per_row:end].plot.bar(ax=axs[i], color=color, width=0.7)
         axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
         if i > 0:
             axs[i].legend().set_visible(False)
@@ -37,5 +32,4 @@ def plot_krippendorff_alpha(k_a: pd.DataFrame, color=None):
             axs[i].legend(loc='upper center', bbox_to_anchor=(0.5, 1.4),
                           fancybox=True, shadow=True, ncol=4)
         plt.grid(axis="x")
-
     return fig
