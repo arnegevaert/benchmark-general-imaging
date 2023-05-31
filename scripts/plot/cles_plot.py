467bc071ea3591bfa82c1f0ee2d3d04a91ba43ae
Arne Gevaert <arne.gevaert@ugent.be>
2023-05-31 15:19:54 +0200

    Added CLES plots

---
+++ scripts/plot/cles_plot.py
@@ -0,0 +1,34 @@
+import argparse
+import matplotlib as mpl
+import numpy as np
+from scipy import stats
+from util.get_dataframes import get_dataframes
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pandas as pd
+import os
+from attrbench.plot.cles_plot import CLESPlot
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("method1", type=str)
+    parser.add_argument("method2", type=str)
+    parser.add_argument("-i", "--in-dir", type=str, default="out/MNIST")
+    parser.add_argument("-o", "--out-dir", type=str, default="plot/MNIST")
+    args = parser.parse_args()
+
+    mpl.use("Agg")
+    np.seterr(all="raise")
+
+    dfs = get_dataframes(args.in_dir, mode="all")
+    cles_plot = CLESPlot(dfs)
+    fig = cles_plot.render(args.method1, args.method2)
+    fig.savefig(
+        os.path.join(
+            args.out_dir,
+            f"pairwise_{args.method1}_{args.method2}.svg",
+        ),
+        bbox_inches="tight",
+    )
+    plt.close(fig)
