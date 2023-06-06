a511cf3617e4f4aace4154056f32ac5a9d522809
Arne Gevaert <arne.gevaert@ugent.be>
2023-06-06 16:31:49 +0200

    Getting all dataframes

--- util/get_dataframes.py
+++ util/get_dataframes.py
@@ -40,11 +40,19 @@ METHODS = {
 
 def _rename_metrics_methods(dfs):
     res = {}
-    for key in METRICS:
-        if key in dfs:
-            df, higher_is_better = dfs[key]
-            df = df.rename(columns=METHODS)
-            res[METRICS[key]] = (df, higher_is_better)
+    for key in dfs:
+        # Get alternative metric name
+        new_key = key
+        for alt_key in METRICS:
+            if key.startswith(alt_key):
+                new_key = key.replace(alt_key, METRICS[alt_key])
+
+        # Rename methods
+        df, higher_is_better = dfs[key]
+        df = df.rename(columns=METHODS)
+
+        # Save result
+        res[new_key] = (df, higher_is_better)
     return res
 
 
@@ -54,15 +62,30 @@ def _subtract_baseline(df, baseline_method):
     return df
 
 
-def get_dataframes(dirname, mode="default", baseline=None):
-    if mode not in ["default", "all"]:
-        raise ValueError("mode must be one of ['default', 'all']")
+def _add_infidelity(dirname, baseline, result):
+    infidelity_object = MetricResult.load(
+        os.path.join(dirname, "infidelity.h5")
+    )
+    for perturbation_generator in ["square", "noisy_baseline"]:
+        df, higher_is_better = infidelity_object.get_df(
+            activation_fn="linear",
+            perturbation_generator=perturbation_generator,
+        )
+        result["infidelity_" + perturbation_generator] = (
+            _subtract_baseline(df, baseline) if baseline is not None else df,
+            higher_is_better,
+        )
 
-    if mode == "default":
-        return _get_default_dataframes(dirname, baseline)
-    elif mode == "all":
-        # return _get_all_dataframes(dirname)
-        return _get_default_dataframes(dirname, baseline)
+
+def _add_max_sensitivity(dirname, baseline, result):
+    max_sensitivity_object = MetricResult.load(
+        os.path.join(dirname, "max_sensitivity.h5")
+    )
+    df, higher_is_better = max_sensitivity_object.get_df()
+    result["max_sensitivity"] = (
+        _subtract_baseline(df, baseline) if baseline is not None else df,
+        higher_is_better,
+    )
 
 
 def _get_default_dataframes(dirname, baseline):
@@ -113,34 +136,81 @@ def _get_default_dataframes(dirname, baseline):
 
     # Add infidelity (perturbation_generator, activation_fn)
     if "infidelity.h5" in available_files:
-        infidelity_object = MetricResult.load(
-            os.path.join(dirname, "infidelity.h5")
-        )
-        for perturbation_generator in ["square", "noisy_baseline"]:
-            df, higher_is_better = infidelity_object.get_df(
-                activation_fn="linear",
-                perturbation_generator=perturbation_generator,
-            )
-            result["infidelity_" + perturbation_generator] = (
-                _subtract_baseline(df, baseline)
-                if baseline is not None
-                else df,
-                higher_is_better,
-            )
+        _add_infidelity(dirname, baseline, result)
 
     # Add max-sensitivity (no arguments)
     if "max_sensitivity.h5" in available_files:
-        max_sensitivity_object = MetricResult.load(
-            os.path.join(dirname, "max_sensitivity.h5")
-        )
-        df, higher_is_better = max_sensitivity_object.get_df()
-        result["max_sensitivity"] = (
-            _subtract_baseline(df, baseline) if baseline is not None else df,
-            higher_is_better,
-        )
+        _add_max_sensitivity(dirname, baseline, result)
 
     return _rename_metrics_methods(result)
 
 
-def _get_all_dataframes(dirname):
-    pass
+def _get_all_dataframes(dirname, baseline):
+    """
+    Returns a dictionary of dataframes, where the keys are the metric names
+    and the values are tuples of (dataframe, higher_is_better).
+
+    Extracts an extended selection of dataframes from each metric:
+    - masker = ["constant", "random", "blur"]
+    - activation_fn = "linear"
+    """
+    result: Dict[str, Tuple[DataFrame, bool]] = {}
+    available_files = os.listdir(dirname)
+    maskers = ["constant", "random", "blurring"]
+
+    # Add simple metrics (masker, activation_fn)
+    simple_metrics = [
+        key
+        for key in METRICS.keys()
+        if key
+        not in ["infidelity", "ms_deletion", "ms_insertion", "max_sensitivity"]
+    ]
+    for metric_name in simple_metrics:
+        filename = metric_name + ".h5"
+        if filename in available_files:
+            result_object = MetricResult.load(os.path.join(dirname, filename))
+            for masker in maskers:
+                df, higher_is_better = result_object.get_df(
+                    masker=masker, activation_fn="linear"
+                )
+                result[f"{metric_name} - {masker}"] = (
+                    _subtract_baseline(df, baseline)
+                    if baseline is not None
+                    else df,
+                    higher_is_better,
+                )
+
+    # Add minimal subset (masker)
+    for metric_name in ["ms_deletion", "ms_insertion"]:
+        filename = metric_name + ".h5"
+        if filename in available_files:
+            result_object = MetricResult.load(os.path.join(dirname, filename))
+            for masker in maskers:
+                df, higher_is_better = result_object.get_df(masker=masker)
+                result[f"{metric_name} - {masker}"] = (
+                    _subtract_baseline(df, baseline)
+                    if baseline is not None
+                    else df,
+                    higher_is_better,
+                )
+
+    # Add infidelity (perturbation_generator, activation_fn)
+    if "infidelity.h5" in available_files:
+        _add_infidelity(dirname, baseline, result)
+
+    # Add max-sensitivity (no arguments)
+    if "max_sensitivity.h5" in available_files:
+        _add_max_sensitivity(dirname, baseline, result)
+    
+    return _rename_metrics_methods(result)
+
+
+def get_dataframes(dirname, mode="default", baseline=None):
+    if mode not in ["default", "all"]:
+        raise ValueError("mode must be one of ['default', 'all']")
+
+    if mode == "default":
+        return _get_default_dataframes(dirname, baseline)
+    elif mode == "all":
+        # return _get_all_dataframes(dirname)
+        return _get_all_dataframes(dirname, baseline)
