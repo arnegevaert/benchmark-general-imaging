3c138b88bacb53b90e4e91c943cee7565c5ba074
Arne Gevaert <arne.gevaert@ugent.be>
2023-06-08 10:13:11 +0200

    Added more plots

---
+++ scripts/benchmark/plot/cluster_plot.py
@@ -0,0 +1,33 @@
+import argparse
+import matplotlib as mpl
+import numpy as np
+from util.get_dataframes import get_dataframes
+from attrbench.plot import ClusterPlot
+import os
+from matplotlib import pyplot as plt
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--in-dir", type=str, default="out/results/MNIST"
+    )
+    parser.add_argument("-o", "--out-dir", type=str, default="out/plots/MNIST")
+    args = parser.parse_args()
+
+    mpl.use("Agg")
+    np.seterr(all="raise")
+
+    if not os.path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
+
+    dfs = get_dataframes(args.in_dir, mode="default")
+    fig = ClusterPlot(dfs).render()
+    fig.savefig(
+        os.path.join(
+            args.out_dir,
+            "cluster.svg",
+        ),
+        bbox_inches="tight",
+    )
+    plt.close(fig)
