c0c79aeca94b6c27f5d5d1fc9726d26a94866c8c
Arne Gevaert <arne.gevaert@ugent.be>
2023-07-19 15:17:55 +0200

    Fix typing and compute_attrs

--- util/attribution/input_x_gradient.py
+++ util/attribution/input_x_gradient.py
@@ -1,9 +1,12 @@
 from captum import attr
+import torch
+from torch import nn
+from attribench import AttributionMethod
 
 
-class InputXGradient:
-    def __init__(self, model):
+class InputXGradient(AttributionMethod):
+    def __init__(self, model: nn.Module):
         self.method = attr.InputXGradient(model)
 
-    def __call__(self, x, target):
-        return self.method.attribute(x, target=target)
+    def __call__(self, batch_x: torch.Tensor, batch_target: torch.Tensor):
+        return self.method.attribute(batch_x, target=batch_target)
--- util/attribution/smooth_grad.py
+++ util/attribution/smooth_grad.py
@@ -1,16 +1,24 @@
 from captum import attr
+import torch
+from torch import nn
+from attribench import AttributionMethod
 
 
-class SmoothGrad:
-    def __init__(self, model, num_samples, stdev):
+class SmoothGrad(AttributionMethod):
+    def __init__(self, model: nn.Module, num_samples: int, stdev: float):
         self.method = attr.NoiseTunnel(attr.Saliency(model))
         self.num_samples = num_samples
         self.stdev = stdev
 
-    def __call__(self, x, target):
-        # sigma = self.noise_level / (x.max()-x.min()) # follows paper more closely, but not perfectly.
+    def __call__(self, batch_x: torch.Tensor, batch_target: torch.Tensor):
+        # follows paper more closely, but not perfectly.
         # in paper the sigma is set per image, here per batch
-        return self.method.attribute(x, target=target, nt_type="smoothgrad",
-                                     nt_samples=self.num_samples,
-                                     nt_samples_batch_size=1,
-                                     stdevs=self.stdev)
+        # sigma = self.noise_level / (x.max()-x.min()) 
+        return self.method.attribute(
+            batch_x,
+            target=batch_target,
+            nt_type="smoothgrad",
+            nt_samples=self.num_samples,
+            nt_samples_batch_size=1,
+            stdevs=self.stdev,
+        )
