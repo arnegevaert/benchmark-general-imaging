2ca10b5568546678c6095be912c907cc4025b0b3
Axel-Jan <axeljanrousseau@gmail.com>
2023-07-24 14:21:22 +0200

    batched version of extremal perturbation

--- util/attribution/extremal_perturbation.py
+++ util/attribution/extremal_perturbation.py
@@ -2,28 +2,24 @@ from attrbench import AttributionMethod
 import torch
 from torch import nn
 import saliency.core as saliency
-from torchray.attribution.extremal_perturbation import extremal_perturbation, contrastive_reward, simple_reward
+from .extremal.extremal_perturbation import extremal_perturbation, contrastive_reward, simple_reward
 
-class ExtremalPerturbation(AttributionMethod):
-    def __init__(self, model: nn.Module,reward = 'contrastive_reward', areas=[0.1])-> None:
+class ExtremalPerturbation_batched(AttributionMethod):
+    def __init__(self, model: nn.Module,reward = 'simple_reward', areas=0.1)-> None:
         super().__init__(model)
-        if reward=='contrastive_reward':
-            self.reward_fuction = contrastive_reward
-        else:
+        if reward=='simple_reward':
             self.reward_fuction = simple_reward
+        else:
+            self.reward_fuction = contrastive_reward
         self.areas=areas
 
     def __call__(self, batch_x: torch.Tensor, batch_target: torch.Tensor) -> torch.Tensor:
-        attributions=[]
-        channels=batch_x.shape[1]
-        for x,y in zip(batch_x,batch_target):
-            with torch.enable_grad():
-                res=extremal_perturbation(self.model,x.unsqueeze(0),int(y),areas=self.areas, reward_func=self.reward_fuction,resize=True)
-            mask = res[0]
-            attributions.append(mask.detach().cpu())
 
-        result=torch.vstack(attributions)
+        channels=batch_x.shape[1]
+        with torch.enable_grad():
+            res=extremal_perturbation(self.model,batch_x,batch_target,areas=self.areas, reward_func=self.reward_fuction,resize=True)
+        result = res[0].detach().cpu()
+
         result=torch.tile(result,dims=[1,channels,1,1])
         
         return result
-        
